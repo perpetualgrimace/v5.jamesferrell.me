4e8e371e7a10795614cfae5c07b37d985c2fef3a
James Ferrell <hello@jamesferrell.me>
2017-09-29 15:45:00 -0400

    add typekit Id, remove unused scripts

--- site/snippets/global-footer-scripts.php
+++ site/snippets/global-footer-scripts.php
@@ -17,7 +17,7 @@
   // typekit
   (function(d) {
     var config = {
-      kitId: 'ugo0ell',
+      kitId: 'bar1poh',
       scriptTimeout: 3000,
       async: true
     },
@@ -26,16 +26,11 @@
 
 
   // Google Analytics
+  /*
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-64615429-1', 'auto');
-  ga('send', 'pageview');
-
-  (function(d,s,i,r) { if (d.getElementById(i)){return;} var n=d.createElement(s),e=d.getElementsByTagName(s)[0]; n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/498501.js'; e.parentNode.insertBefore(n, e); })(document,"script","hs-analytics",300000);
+  */
 
 </script>
-
-<!-- <script type="text/javascript" src="//js.leadin.com/js/v1/2123341.js" id="LeadinEmbed-2123341" crossorigin="use-credentials" async defer></script> -->
