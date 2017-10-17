3bfc527d191b52879571ec1acdeea36a011155e5
James Ferrell <hello@jamesferrell.me>
2017-10-17 18:00:55 -0400

    device illustrations [https://codepen.io/perpetualgrimace/pen/jGXrER]

---
+++ site/snippets/device-laptop.php
@@ -0,0 +1,14 @@
+<?
+
+// logic here
+
+?>
+
+<div class="device-container laptop-container">
+  <div class="device-shell laptop-shell"></div>
+  <div class="device-screen laptop-screen">
+    <img src="http://fillmurray.com/1048/656">
+  </div>
+  <div class="laptop-bottom"></div>
+  <div class="laptop-bottom-bevel"></div>
+</div>
---
+++ site/snippets/device-mobile.php
@@ -0,0 +1,12 @@
+<?
+
+// logic here
+
+?>
+
+<div class="device-container phone-container">
+  <div class="device-shell phone-shell"></div>
+  <div class="device-screen phone-screen">
+    <img src="http://fillmurray.com/288/512">
+  </div>
+</div>
